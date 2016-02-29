26f271b70e45026384c009b927e26dff2f9d89c3
Eugene Nemchenko <n.eugene536@gmail.com>
2016-02-29 09:57:31 +0300

    just

--- http_server.cpp
+++ http_server.cpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "http_server.hpp"
 #include <iostream>
 #include <exception>
@@ -9,13 +10,10 @@
 
 namespace ip = boost::asio::ip;
 
-HttpServer::HttpServer(const std::string& host, unsigned short port, const std::string& directory) 
-    : host_(host)
-    , port_(port)
-    , directory_(directory)
-    , acceptor_(io_service_, ip::tcp::endpoint(ip::address::from_string(host), port))
-    , log_("/home/eugene/university/3 course/stepic/multithreading_c++/http_server/log")
-{
+HttpServer::HttpServer(const std::string &host, unsigned short port, const std::string &directory)
+        : host_(host), port_(port), directory_(directory),
+          acceptor_(io_service_, ip::tcp::endpoint(ip::address::from_string(host), port)),
+          log_("/home/eugene/university/3 course/stepic/multithreading_c++/http_server/log") {
     acceptor_.set_option(ip::tcp::socket::reuse_address(true));
 }
 
@@ -26,10 +24,10 @@ void HttpServer::run() {
     while (true) {
         {
             std::unique_lock<std::mutex> lk(m_);
-            cv_.wait(lk, 
-                [&]() -> bool {
-                    return !q_.empty(); 
-                }
+            cv_.wait(lk,
+                     [&]() -> bool {
+                         return !q_.empty();
+                     }
             );
             task = q_.front();
             q_.pop();
@@ -38,7 +36,7 @@ void HttpServer::run() {
     }
 }
 
-void HttpServer::readHandler(Client* client, const boost::system::error_code & err, std::size_t read_bytes) {
+void HttpServer::readHandler(Client *client, const boost::system::error_code &err, std::size_t read_bytes) {
     using namespace boost::asio;
     using std::placeholders::_1;
     using std::placeholders::_2;
@@ -60,13 +58,17 @@ void HttpServer::readHandler(Client* client, const boost::system::error_code & e
             file.erase(file.find_first_of("?"));
         }
         log_ << "req2: " << file << endl;
-        
+
         log_ << "file: " << file << endl;
 
         log_ << "her" << endl;
         std::string text;
-        boost::filesystem::path path(file);
-        if (!exists(path) || is_directory(path)) {
+        bool flag;
+        {
+            boost::filesystem::path path(file);
+            flag = !exists(path) || is_directory(path);
+        }
+        if (flag) {
             log_ << "!boost" << endl;
             text = "HTTP/1.0 404 FAIL\r\nContent-Length:0\r\n\r\n";
             write(client->sock_, buffer(text));
@@ -74,17 +76,17 @@ void HttpServer::readHandler(Client* client, const boost::system::error_code & e
             log_ << "boost" << endl;
             ifstream result(file);
             text = std::string((std::istreambuf_iterator<char>(result)),
-                                std::istreambuf_iterator<char>());
+                               std::istreambuf_iterator<char>());
             log_ << "her2" << endl;
 
-            text = "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + 
-                   std::to_string(text.size()) + 
+            text = "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nContent-Length: " +
+                   std::to_string(text.size()) +
                    "\r\n\r\n" + text;
 
-            async_write(client->sock_, buffer(text), [](boost::system::error_code, std::size_t){});
+            async_write(client->sock_, buffer(text), [](boost::system::error_code, std::size_t) { });
         }
         log_ << "text: " << text << endl;
-    } catch (exception& e) {
+    } catch (exception &e) {
         log_ << "exc: 83: " << e.what() << endl;
         write(client->sock_, buffer("HTTP/1.0 404 FAIL\r\n\r\n"));
     }
@@ -93,13 +95,13 @@ void HttpServer::readHandler(Client* client, const boost::system::error_code & e
     //async_read_until(client->sock_, client->buff_, "\r\n", std::bind(&HttpServer::readHandler, this, client, _1, _2));
 }
 
-void HttpServer::acceptHandler(Client* client, const boost::system::error_code & err) {
+void HttpServer::acceptHandler(Client *client, const boost::system::error_code &err) {
     using namespace boost::asio;
     using std::placeholders::_1;
     using std::placeholders::_2;
 
     async_read_until(client->sock_, client->buff_, "\r\n", std::bind(&HttpServer::readHandler, this, client, _1, _2));
-    
+
     clients_.emplace_back(io_service_);
     acceptor_.async_accept(clients_.back().sock_, std::bind(&HttpServer::acceptHandler, this, &clients_.back(), _1));
 }
@@ -113,17 +115,17 @@ void HttpServer::start() {
 
         using std::placeholders::_1;
         clients_.emplace_back(io_service_);
-        acceptor_.async_accept(clients_.back().sock_, std::bind(&HttpServer::acceptHandler, this, &clients_.back(), _1));
+        acceptor_.async_accept(clients_.back().sock_,
+                               std::bind(&HttpServer::acceptHandler, this, &clients_.back(), _1));
 
         io_service_.run();
-    } catch (std::exception& e) {
+    } catch (std::exception &e) {
         std::cerr << "Exception: " << e.what() << "\n";
     }
 }
 
 
-HttpServer::Client::Client(boost::asio::io_service& io_service)
-    : sock_(io_service)
-{}
+HttpServer::Client::Client(boost::asio::io_service &io_service)
+        : sock_(io_service) { }
 
 //void HttpServer::ready_to_read(
